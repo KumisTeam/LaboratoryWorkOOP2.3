d854ca1694d7c43d1ffd9c30a785ac0304612c07
MGromov <maksim_gromov_2012@mail.ru>
2016-04-17 17:57:45 +0300

    abstract Strings

---
+++ LaboratoryWorkOOP2.3(sharp)/LaboratoryWorkOOP2.3(sharp)/Strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWorkOOP2._3_sharp_
+{
+    abstract class Strings
+    {
+        public abstract int StringLengnth();
+        public abstract void MakeSmallString();
+        public abstract void Show();
+    }
+}
