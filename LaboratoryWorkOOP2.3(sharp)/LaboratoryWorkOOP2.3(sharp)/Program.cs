63591d3c032c193b104a595328a7e184e681dcf9
MGromov <maksim_gromov_2012@mail.ru>
2016-04-17 18:46:40 +0300

    main

--- LaboratoryWorkOOP2.3(sharp)/LaboratoryWorkOOP2.3(sharp)/Program.cs
+++ LaboratoryWorkOOP2.3(sharp)/LaboratoryWorkOOP2.3(sharp)/Program.cs
@@ -10,6 +10,18 @@ namespace LaboratoryWorkOOP2._3_sharp_
     {
         static void Main(string[] args)
         {
+            Symbols s = new Symbols("sdgdsggavgdgsavgva");
+            s.Show();
+            s.RemoveFromOddPosition();
+            s.MakeSmallString();
+            s.Show();
+
+            int[] arr = { 2, 5, 12, 53, 532, 32, 134, };
+            Digits d = new Digits(arr);
+            d.Show();
+            d.RemoveFromOddPosition();
+            d.MakeSmallString();
+            d.Show();
         }
     }
 }
