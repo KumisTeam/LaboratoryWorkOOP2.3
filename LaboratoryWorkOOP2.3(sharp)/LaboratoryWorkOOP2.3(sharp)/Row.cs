51e75855a457033b38357a7de961a8bb1da0c629
MGromov <maksim_gromov_2012@mail.ru>
2016-04-20 00:16:52 +0300

    abstract class Row

---
+++ LaboratoryWorkOOP2.3(sharp)/LaboratoryWorkOOP2.3(sharp)/Row.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWorkOOP2._3_sharp_
+{
+    abstract class Row
+    {
+        public abstract int GetSize();
+
+        public abstract void DeleteSmth();
+
+        public abstract void Show();
+    }
+}
