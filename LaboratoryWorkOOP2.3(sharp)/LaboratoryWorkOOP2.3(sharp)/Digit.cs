3e7b86f6be58005bbaae1ff91058143d5129203d
MGromov <maksim_gromov_2012@mail.ru>
2016-04-20 00:40:20 +0300

    class digit

---
+++ LaboratoryWorkOOP2.3(sharp)/LaboratoryWorkOOP2.3(sharp)/Digit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWorkOOP2._3_sharp_
+{
+    class Digit : Row
+    {
+        private List<int> digits;
+
+        public Digit(string usrDigits)
+        {
+            digits = new List<int>();
+            char[] tmpArr = usrDigits.ToCharArray();
+            foreach (var elem in tmpArr)
+                if (char.IsDigit(elem))
+                    digits.Add(Convert.ToInt32(elem));
+        }
+
+        public override void DeleteSmth()
+        {
+            for (int i = 0; i < digits.Count; i++)
+                digits.Remove(5);
+        }
+
+        public override int GetSize()
+        {
+            return digits.Count;
+        }
+
+        public override void Show()
+        {
+            foreach (var elem in digits)
+                Console.Write(elem);
+            Console.WriteLine();
+        }
+    }
+}
